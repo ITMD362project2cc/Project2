c7157fe6e71942c50c0c71ce4c853b90c29414ab
Christopher Siegfried <35548245+csiegfri@users.noreply.github.com>
2018-11-07 15:53:06 -0600

    copied proj 1 script

---
+++ script/script.js
@@ -0,0 +1,6 @@
+Thanks = function(){
+  let name = document.getElementById("handle").value;
+  let email = document.getElementById("email").value;
+  result = "Congratulations, " + name + ", you're in. Expect an email at " + email + " in the near future.";
+  alert(result);  
+};
